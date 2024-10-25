b91bc2f14a6021d2a0e1359e7223c070c4d35f0b
Chiyo Yuki <1911345567@qq.com>
2024-10-25 21:06:26 +0800

    create template

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/**
+ *    author:  ChiyoYuki
+ **/
+ 
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+ 
+#define ll long long
+#define ull unsigned long long
+#define vll vector<long long>
+#define vvll vector<vector<long long> >
+#define pll2 pair<long long, long long>
+#define vd vector<double>
+#define vvd vector<vector<double> >
+#define pd2 pair<double, double>
+ 
+using namespace std;
+ 
+void solve(void);
+ 
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    unsigned long long T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
+ 
+void solve(void)
+{
+    ll i = 0, j = 0;
+    ll n, m, sum = 0, ans = 0;
+ 
+    cout << endl;
+    return;
+}

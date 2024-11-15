d4a7f34b6a54d2f923d4bac3b3c23135df8088db
ChiyoYuki <1911345567@qq.com>
2024-11-16 01:37:31 +0800

    修改：     main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
  **/
 
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
 
 typedef long long ll;
 typedef unsigned long long ull;
@@ -15,20 +18,19 @@ typedef vector<long long> vll;
 typedef vector<vector<long long>> vvll;
 typedef vector<pair<long long,long long>> vpll2;
 
-const int inf=0x3f3f3f3f;
-const int mod=998244353;
-const int MOD=1000000009;
-
-using namespace std;
+const int intinf=0x3f3f3f3f;
+const int llinf=0x3f3f3f3f3f3f3f3f;
+const int mod9=998244353;
+const int moda=1000000007;
 
 void solve(void);
 
 int main(void)
 {
     ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-    unsigned long long T = 1;
-    cin >> T;
-    while (T--)
+    unsigned long long _ = 1;
+    cin >> _;
+    while (_--)
         solve();
     return 0;
 }
@@ -37,8 +39,12 @@ void solve(void)
 {
     ll i = 0, j = 0;
     ll n, m, sum = 0, ans = 0;
+    
+    // cin>>n;
+    // vll a(n);
+    // for(i=0;i<n;i++) cin>>a[i];
 
-    cout << endl;
+    cout << '\n';
     return;
 }
 

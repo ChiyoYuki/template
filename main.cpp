c3e84f191489fdc6b54e6f41e4ebd929cbf3fd2c
ChiyoYuki <1911345567@qq.com>
2024-12-14 22:07:35 +0800

    新文件：   .vscode/tasks.json 	新文件：   main 	修改：     main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-// using namespace interactive;
 
 #define st first
 #define nd second
+#define elif else if
 
 typedef long long ll;
 typedef unsigned long long ull;
@@ -20,29 +20,21 @@ typedef vector<char> vc;
 typedef vector<ll> vll;
 typedef vector<vector<ll>> vvll;
 typedef vector<pair<ll, ll>> vpll2;
+typedef string str;
 
 const int intinf = 0x3f3f3f3f;
 const int mod9 = 998244353;
 const int moda = 1000000007;
 
-namespace interactive
+namespace i11e
 {
-void ask(ll a);
-void ask(ll a, ll b);
-void ans(ll a);
-void ans(string s);
-void ans(vll a);
-}
-void solve(void);
-
-int main(void)
-{
-    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-    unsigned long long _ = 1;
-    cin >> _;
-    while (_--)
-        solve();
-    return 0;
+    template <typename __Tp>
+    void ask(__Tp a);
+    template <typename __Tp>
+    void ask(__Tp a, __Tp b);
+    void ans(ll a);
+    void ans(str s);
+    void ans(vll a);
 }
 
 void solve(void)
@@ -63,35 +55,47 @@ void solve(void)
 
 */
 
-void interactive::ask(ll a)
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    unsigned long long _ = 1;
+    cin >> _;
+    while (_--)
+        solve();
+    return 0;
+}
+
+template <typename __Tp>
+void i11e::ask(__Tp a)
 {
     cout << "? " << a << endl;
     cout.flush();
     return;
 }
 
-void interactive::ask(ll a, ll b)
+template <typename __Tp>
+void i11e::ask(__Tp a, __Tp b)
 {
     cout << "? " << a << ' ' << b << endl;
     cout.flush();
     return;
 }
 
-void interactive::ans(ll a)
+void i11e::ans(ll a)
 {
     cout << "! " << a << endl;
     cout.flush();
     return;
 }
 
-void interactive::ans(string s)
+void i11e::ans(str s)
 {
     cout << "! " << s << endl;
     cout.flush();
     return;
 }
 
-void interactive::ans(vll a)
+void i11e::ans(vll a)
 {
     cout << "!";
     for (auto ii = a.begin(); ii != a.end(); ii++)
